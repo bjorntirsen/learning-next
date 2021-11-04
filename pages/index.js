6aa77b8662c69bb24583d145c0a0e6576caaca0d
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-04 09:51:15 +0100

    :sparkles: Add links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-  return <h1>The Home Page</h1>;
+  return (
+    <>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/alink'>a link</Link>
+        </li>
+        <li>
+          <Link href='/news/anotherlink'>another link</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default HomePage;
